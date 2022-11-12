c5050a5a897037772da4c8edaf4d0a13b0ec834c
moh3a <aitabdelmalekma@gmail.com>
2022-11-12 22:56:48 +0100

    added first part of testing

---
+++ adder/target/debug/deps/adder-42cf3de66a1e25fe.d
@@ -0,0 +1,7 @@
+D:\the_rust_lang_tutorial\adder\target\debug\deps\adder-42cf3de66a1e25fe.rmeta: src\lib.rs
+
+D:\the_rust_lang_tutorial\adder\target\debug\deps\libadder-42cf3de66a1e25fe.rlib: src\lib.rs
+
+D:\the_rust_lang_tutorial\adder\target\debug\deps\adder-42cf3de66a1e25fe.d: src\lib.rs
+
+src\lib.rs:
---
+++ adder/target/debug/deps/adder-efb263bcc2fe958d.d
@@ -0,0 +1,5 @@
+D:\the_rust_lang_tutorial\adder\target\debug\deps\adder-efb263bcc2fe958d.exe: src\lib.rs
+
+D:\the_rust_lang_tutorial\adder\target\debug\deps\adder-efb263bcc2fe958d.d: src\lib.rs
+
+src\lib.rs:
